8f7010e1424c3b323d2de4af5bdc7e0bbb4d2f59
Henri Bergius <henri.bergius@iki.fi>
2017-11-06 15:32:20 +0100

    Port to ES6 class

--- runtime/network.js
+++ runtime/network.js
@@ -1,91 +1,86 @@
-var WebSocketServer = require('websocket').server;
-var Base = require('noflo-runtime-base');
+const { server: WebSocketServer } = require('websocket');
+const Base = require('noflo-runtime-base');
 
-function WebSocketRuntime (options) {
-  if (!options) {
-    options = {};
-  }
-  this.connections = [];
-  if (options.catchExceptions) {
-    process.on('uncaughtException', function (err) {
-      this.connections.forEach(function (connection) {
-        this.send('network', 'error', err, {
-          connection: connection
-        });
-        if (err.stack) {
-          console.error(err.stack);
-        } else {
-          console.error('Error: ' + err.toString());
-        }
+class WebSocketRuntime extends Base {
+  constructor(options = {}) {
+    super(options);
+    this.connections = [];
+    if (options.catchExceptions) {
+      process.on('uncaughtException', function (err) {
+        this.connections.forEach(function (connection) {
+          this.send('network', 'error', err, {
+            connection: connection
+          });
+          if (err.stack) {
+            console.error(err.stack);
+          } else {
+            console.error('Error: ' + err.toString());
+          }
+        }.bind(this));
       }.bind(this));
-    }.bind(this));
+    }
+
+    if (options.captureOutput) {
+      this.startCapture();
+    }
   }
 
-  if (options.captureOutput) {
-    this.startCapture();
+  send(protocol, topic, payload, context) {
+    if (!context.connection || !context.connection.connected) {
+      return;
+    }
+    if (payload instanceof Error) {
+      payload = {
+        message: payload.message,
+        stack: payload.stack
+      };
+    }
+    context.connection.sendUTF(JSON.stringify({
+      protocol: protocol,
+      command: topic,
+      payload: payload
+    }));
+    super.send(protocol, topic, payload, context);
   }
-  this.prototype.constructor.apply(this, arguments);
-  this.receive = this.prototype.receive;
-  this.canDo = this.prototype.canDo;
-  this.getPermitted = this.prototype.getPermitted;
-}
-WebSocketRuntime.prototype = Base;
-WebSocketRuntime.prototype.send = function (protocol, topic, payload, context) {
-  if (!context.connection || !context.connection.connected) {
-    return;
-  }
-  if (payload instanceof Error) {
-    payload = {
-      message: payload.message,
-      stack: payload.stack
-    };
-  }
-  context.connection.sendUTF(JSON.stringify({
-    protocol: protocol,
-    command: topic,
-    payload: payload
-  }));
-  this.prototype.send.apply(this, arguments);
-};
 
-WebSocketRuntime.prototype.sendAll = function (protocol, topic, payload, context) {
-
-  this.connections.forEach(function(connection) {
-    this.send(protocol, topic, payload, {
-      connection: connection
-    });
-  }.bind(this));
-};
-
-WebSocketRuntime.prototype.startCapture = function () {
-  this.originalStdOut = process.stdout.write;
-  process.stdout.write = function (string, encoding, fd) {
-    this.connections.forEach(function (connection) {
-      this.send('network', 'output', {
-        message: string.replace(/\n$/, '')
-      }, {
+  sendAll(protocol, topic, payload, context) {
+    this.connections.forEach((connection) => {
+      this.send(protocol, topic, payload, {
         connection: connection
       });
-    }.bind(this));
-  }.bind(this);
-};
-
-WebSocketRuntime.prototype.stopCapture = function () {
-  if (!this.originalStdOut) {
-    return;
+    });
   }
-  process.stdout.write = this.originalStdOut;
-};
+
+  startCapture() {
+    this.originalStdOut = process.stdout.write;
+    process.stdout.write = function (string, encoding, fd) {
+      this.connections.forEach(function (connection) {
+        this.send('network', 'output', {
+          message: string.replace(/\n$/, '')
+        }, {
+          connection: connection
+        });
+      }.bind(this));
+    }.bind(this);
+  }
+
+  stopCapture() {
+    if (!this.originalStdOut) {
+      return;
+    }
+    process.stdout.write = this.originalStdOut;
+  }
+}
 
 module.exports = function (httpServer, options) {
-  var wsServer = new WebSocketServer({
+  const wsServer = new WebSocketServer({
     httpServer: httpServer
   });
 
-  var runtime = new WebSocketRuntime(options);
-  var handleMessage = function (message, connection) {
+  const runtime = new WebSocketRuntime(options);
+  const handleMessage = function (message, connection) {
     if (message.type == 'utf8') {
-      var contents;
+      let contents;
       try {
         contents = JSON.parse(message.utf8Data);
       } catch (e) {
@@ -103,8 +98,8 @@ module.exports = function (httpServer, options) {
   };
 
   wsServer.on('request', function (request) {
-    var subProtocol = (request.requestedProtocols.indexOf("noflo") != -1) ? "noflo" : null;
-    var connection = request.accept(subProtocol, request.origin);
+    const subProtocol = (request.requestedProtocols.indexOf("noflo") != -1) ? "noflo" : null;
+    const connection = request.accept(subProtocol, request.origin);
     runtime.connections.push(connection);
     connection.on('message', function (message) {
       handleMessage(message, connection);
